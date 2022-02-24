1d8b4e1a3cc2a236a36280ab3d5627e433de4e6b
Jose Sántiz <joseluis.santizgomez123@gmail.com>
2022-02-24 17:07:19 -0600

    dependencia thymeleaf

--- src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -33,11 +34,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http
+    		.formLogin()
+    		.loginPage("/login")
+    		.defaultSuccessUrl("/")
+    		.failureUrl("/login?error=true")
+    		.permitAll()
+    			.and()
 			.authorizeRequests()
+			//.antMatchers(HttpMethod.GET,"/api/users/*").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
-			.httpBasic();
+			.httpBasic()
+				.and()
+			.csrf().disable()
+				.logout()
+				.logoutSuccessUrl("/login?logout=true")
+				.invalidateHttpSession(true)
+				.permitAll();
 	}
 
 }
---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.repository.UserPassRepository;
+
+
+@Controller
+public class LoginController {
+	
+	private UserPassRepository userPassRepository;
+	
+	
+	public LoginController(UserPassRepository userPassRepository) {
+		this.userPassRepository = userPassRepository;
+	}
+
+	@GetMapping("/greeting")
+	public String greeting(Model model) {		
+		model.addAttribute("usuario", userPassRepository.findAll());
+		return "inicio";
+	}
+	
+	@GetMapping("/login")
+	public String login() {		
+		return "login";
+	}
+
+}
